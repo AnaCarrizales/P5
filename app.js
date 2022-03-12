a7877c06678a23856694df1b066feba7120bb1b0
Ana Carrizales <acarrizales2@ucol.mx>
2022-03-12 16:55:10 -0600

    uploaded files

---
+++ app.js
@@ -0,0 +1,23 @@
+var Emitter = require('./emitter');
+
+var emtr = new Emitter();
+
+emtr.on('greet', () => {
+    console.log('Somewhere, someone said hello.');
+});
+
+emtr.on('greet', () => {
+    console.log('A greeting occurred!');
+});
+
+console.log('Hello!');
+emtr.emit('greet');
+
+//Se muestra el mensaje de Hello y los dos mensajes de cada función que contiene greet
+
+emtr.on('jump', () => {
+    console.log('someone jumped!');
+});
+
+console.log(emtr);// imprime los eventos de las funciones que contiene greet como funciones anónimas
+emtr.emit('jump');//imprime el mensaje de la funciónde jump
